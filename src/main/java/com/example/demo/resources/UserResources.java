d20e0901f3094cbe04e0d416d63d2ad187a950f8
LucasManciniDaSilva <lucasmancini1997@gmail.com>
2019-08-28 00:37:12 -0300

    Connecting with Mongodb and create repository and services

--- src/main/java/com/example/demo/resources/UserResources.java
+++ src/main/java/com/example/demo/resources/UserResources.java
@@ -1,15 +1,15 @@
 package com.example.demo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.User;
+import com.example.demo.services.UserService;
 
 
 
@@ -17,12 +17,14 @@ import com.example.demo.domain.User;
 @RequestMapping(value="/users")
 public class UserResources {
 	
+	
+	@Autowired
+	private UserService service;
+	
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria Brown", "Maria@gmail.com");
-		User alex = new User("2", "Alex Green", "Alex@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
